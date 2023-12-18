0e9649827be53147901aafe5c651cfe2cd187f0b
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2023-12-18 10:38:32 -0300

    add indicators

--- index.js
+++ index.js
@@ -3,30 +3,55 @@ document.addEventListener("DOMContentLoaded", function () {
   const prevBtn = document.getElementById("prevBtn");
   const nextBtn = document.getElementById("nextBtn");
   const images = document.querySelectorAll(".image");
+  const indicatorsContainer = document.querySelector(".indicators-container");
 
-  let currentPosition = 0;
+  let currentIndex = 0;
   const imagesToShow = 4;
 
+  for (let i = 0; i < images.length / imagesToShow; i++) {
+    const indicator = document.createElement("div");
+    indicator.classList.add("indicator");
+    indicator.addEventListener("click", function () {
+      currentIndex = i * imagesToShow;
+      updateCarousel();
+    });
+    indicatorsContainer.appendChild(indicator);
+  }
+
+  updateIndicators();
+
   nextBtn.addEventListener("click", function () {
-    if (currentPosition < images.length - imagesToShow) {
-      currentPosition++;
+    if (currentIndex < images.length - imagesToShow) {
+      currentIndex++;
     } else {
-      currentPosition = 0;
+      currentIndex = 0;
     }
     updateCarousel();
   });
 
   prevBtn.addEventListener("click", function () {
-    if (currentPosition > 0) {
-      currentPosition--;
+    if (currentIndex > 0) {
+      currentIndex--;
     } else {
-      currentPosition = images.length - imagesToShow;
+      currentIndex = images.length - imagesToShow;
     }
     updateCarousel();
   });
 
   function updateCarousel() {
-    const translateValue = -currentPosition * (100 / imagesToShow) + "%";
+    const translateValue = -currentIndex * (100 / imagesToShow) + "%";
     carousel.style.transform = "translateX(" + translateValue + ")";
+    updateIndicators();
+  }
+
+  function updateIndicators() {
+    const indicators = document.querySelectorAll(".indicator");
+    indicators.forEach((indicator, i) => {
+      if (i * imagesToShow <= currentIndex && currentIndex < (i + 1) * imagesToShow) {
+        indicator.classList.add("active");
+      } else {
+        indicator.classList.remove("active");
+      }
+    });
   }
 });
